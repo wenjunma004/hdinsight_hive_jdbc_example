18119c7751140d74591ac8450da915b26b44349e
Wenjun Ma <wenjm@microsoft.com>
2020-04-18 17:00:07 -0700

    remove up

--- src/main/java/com/microsoft/hdinsight/HiveClient.java
+++ src/main/java/com/microsoft/hdinsight/HiveClient.java
@@ -10,12 +10,13 @@ public class HiveClient {
 
     private static String HiveDriverName = "org.apache.hive.jdbc.HiveDriver";
     private static String jdbcUrl = "jdbc:hive2://zk0-wenjml.e5hngsuf0p5etjvfhkh113wsyd.xx.internal.cloudapp.net:2181,zk2-wenjml.e5hngsuf0p5etjvfhkh113wsyd.xx.internal.cloudapp.net:2181,zk4-wenjml.e5hngsuf0p5etjvfhkh113wsyd.xx.internal.cloudapp.net:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2-interactive";
-    private static String userName = "admin";
-    private static String userPass = "$Amsung762";
+//    private static String userName = "admin";
+//    private static String userPass = "$Amsung762";
 
     public static void main(String[] args) throws Exception {
         Class.forName(HiveDriverName);
-        Connection con = DriverManager.getConnection(jdbcUrl, userName, userPass);
+//        Connection con = DriverManager.getConnection(jdbcUrl, userName, userPass); // no need username and password
+        Connection con = DriverManager.getConnection(jdbcUrl);
         System.out.println("\nGot Connection: " + con);
         System.out.println("\nRun show tables command and listing 'default' Database tables of hive.");
         Statement stmt = con.createStatement();
